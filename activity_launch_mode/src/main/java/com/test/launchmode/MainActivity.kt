9999552e75d67960f1399a00af41b2c96c65681d
liwei <wei.li@downjoy.com>
2020-06-01 10:20:34 +0800

    添加匹配规则并成功启动目标Activity

--- activity_launch_mode/src/main/java/com/test/launchmode/MainActivity.kt
+++ activity_launch_mode/src/main/java/com/test/launchmode/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.test.launchmode
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import java.lang.Exception
 
 class MainActivity : BaseActivity() {
 
     private var button: Button? = null
+    private var button2: Button? = null
+
+    private val MY_ACTION = "com.test.target.myAction"
+    private val MY_CATEGORY = "com.test.target.myCategory"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,20 @@ class MainActivity : BaseActivity() {
         button?.setOnClickListener {
             val intent = Intent(this,SecondActivity::class.java)
             startActivity(intent)
+        }
+        button2 = findViewById(R.id.button2)
+        button2?.setOnClickListener {
+            try{
+                val intent = Intent()
+                intent.action = MY_ACTION
+                intent.addCategory(MY_CATEGORY)
+                intent.setDataAndType(Uri.parse("content://abc"),"image/png")
+                intent.putExtra("XXX","hello target")
+                startActivity(intent)
+                println("找到目标Activity，启动成功")
+            }catch(e:Exception){
+                println("没有找到目标Activity，启动失败")
+            }
         }
     }
 }
--- activity_launch_mode/src/main/java/com/test/launchmode/TargetActivity.kt
+++ activity_launch_mode/src/main/java/com/test/launchmode/TargetActivity.kt
@@ -2,6 +2,8 @@ package com.test.launchmode
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.TextView
 
 /**
  * desc: 需要匹配过滤信息的Activity
@@ -10,8 +12,18 @@ import android.os.Bundle
  */
 class TargetActivity : Activity() {
 
+    private var tvText : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_target)
+
+        tvText = findViewById(R.id.getResult);
+        val myIntent = intent
+        tvText?.text = if(TextUtils.isEmpty(myIntent.getStringExtra("XXX"))){
+            "我是targetActivity"
+        }else{
+            myIntent.getStringExtra("XXX")
+        }
     }
 }
