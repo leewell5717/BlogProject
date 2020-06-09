cb05ed232c3c854aa4759b98ec7db5b57bb563d8
liwei <wei.li@downjoy.com>
2020-06-09 10:20:36 +0800

    在ServiceTwo中添加binder对象

--- service_basic_use/src/main/java/com/test/service/use/service/ServiceTwo.kt
+++ service_basic_use/src/main/java/com/test/service/use/service/ServiceTwo.kt
@@ -2,29 +2,16 @@ package com.test.service.use.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
-import java.util.*
-import kotlin.concurrent.timerTask
 
-class ServiceTwo : Service(){
+class ServiceTwo : Service() {
 
-    private var timer: Timer = Timer()
-    private var timerTask: TimerTask? = null
-    private var count = 0
+    var myBinder = MyBinder()
 
     override fun onCreate() {
         super.onCreate()
         println("ServiceTwo-->onCreate")
-
-        timerTask = timerTask {
-            count++
-            println(count)
-            if(count > 10){
-                println("counr超过了10，停止自己")
-                stopSelf()
-            }
-        }
-        timer.schedule(timerTask,1000,1000)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -34,7 +21,8 @@ class ServiceTwo : Service(){
 
     override fun onBind(intent: Intent?): IBinder? {
         println("ServiceTwo-->onBind")
-        return null
+        return myBinder
+//        return null
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
@@ -45,6 +33,11 @@ class ServiceTwo : Service(){
     override fun onDestroy() {
         println("ServiceTwo-->onDestroy")
         super.onDestroy()
-        timer.cancel()
+    }
+
+    class MyBinder : Binder() {
+        fun getMessage(msg: String) = println("输入的消息是：$msg")
+        fun getAddition(a: Int, b: Int) = println("$a + $b 的值是${a + b}")
+        fun getSubtraction(a: Int, b: Int) = println("$a - $b 的值是${a - b}")
     }
 }
