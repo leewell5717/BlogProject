154b3060101324b3f2919d22b117e1e7bda3f7ee
liwei <wei.li@downjoy.com>
2020-07-10 14:41:59 +0800

    添加工厂方法模式代码

--- design_mode_use/src/main/java/com/test/design/mode/use/MainActivity.kt
+++ design_mode_use/src/main/java/com/test/design/mode/use/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import com.test.design.mode.use.builder_mode.BuilderModeActivity
+import com.test.design.mode.use.factory_method_mode.FactoryMethodModeActivity
 import com.test.design.mode.use.prototype_mode.PrototypeModeJavaActivity
 import com.test.design.mode.use.prototype_mode.PrototypeModeKotlinActivity
 import com.test.design.mode.use.singleton_mode.SingletonModeActivity
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     private var singletonModeBtn : Button? = null
     private var builderModeBtn : Button? = null
     private var prototypeModeBtn : Button? = null
+    private var factoryMethodModeBtn : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +28,8 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         builderModeBtn?.setOnClickListener(this)
         prototypeModeBtn = findViewById(R.id.prototypeModeBtn)
         prototypeModeBtn?.setOnClickListener(this)
+        factoryMethodModeBtn = findViewById(R.id.factoryMethodModeBtn)
+        factoryMethodModeBtn?.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -38,6 +42,8 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
                 //这里切换成java的原型模式/kotlin的原型模式
 //                Intent(this, PrototypeModeJavaActivity::class.java)
                 Intent(this, PrototypeModeKotlinActivity::class.java)
+            }factoryMethodModeBtn -> {
+                Intent(this, FactoryMethodModeActivity::class.java)
             }else -> {
                 Intent(this, MainActivity::class.java)
             }
---
+++ design_mode_use/src/main/java/com/test/design/mode/use/factory_method_mode/FactoryMethodModeActivity.java
@@ -0,0 +1,17 @@
+package com.test.design.mode.use.factory_method_mode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.test.design.mode.use.R;
+
+/**
+ * Description: 工厂方法模式
+ */
+public class FactoryMethodModeActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factory_method_mode);
+    }
+}
